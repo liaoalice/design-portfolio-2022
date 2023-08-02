45ff426f175af91f23be4e2584eb202e06e37dd9
Alice Liao <alice.g.liao@vanderbilt.edu>
2023-08-01 22:54:35 -0400

    about me

--- src/About.jsx
+++ src/About.jsx
@@ -10,9 +10,16 @@ export default function About() {
         <div class="about-text">
           <h2>Hi, I'm Alice!</h2>
           <p>I work on the design system team at an enterprise software company.</p>
-          <p>Prior to my current role, I graduated from Vanderbilt University. There, I majored in English, minored in Computer Science, created digital posters for the law school, led design for <a href="https://vandyhacks.org/">a collegiate hackathon</a> with 650 attendees, and performed <a href="https://en.wikipedia.org/wiki/S%C5%8Dran_Bushi">sōran bushi</a> at a Lunar New Year festival.</p>
-          <p>Outside of design, I enjoy reading, writing, drawing, lifting, making music, and <a href="https://www.newyorker.com/magazine/2020/05/25/where-do-eels-come-from">wondering about European eels.</a></p>
-          <hr />
+          <p>Prior to my current role, I designed graphics at an agency. And prior to that role, I was a student at Vanderbilt University. There, I majored in English, minored in Computer Science, created digital posters for the law school, led design for <a href="https://vandyhacks.org/">a collegiate hackathon</a> with 650 attendees, and performed <a href="https://en.wikipedia.org/wiki/S%C5%8Dran_Bushi">sōran bushi</a> at a Lunar New Year festival.</p>
+          <p>Outside of design, I like to:</p>
+          <ul>
+            <li>Read Seth Dickinson, Jane Austen, and Ursula K. Le Guin</li>
+            <li>Draw on my iPad</li>
+            <li>Attempt to master the electric bass</li>
+            <li>Weightlift</li>
+            <li>Simmer a big pot of Hong Kong-style borscht</li>
+          </ul>
+          {/* <hr /> */}
           {/* <p>I was born in the Year of the Rabbit and named in English for a certain girl who encounters a white rabbit, which is why I've styled this site after <a href="https://en.wikipedia.org/wiki/White_Rabbit_(candy)">White Rabbit candy</a>. The red characters at the top left of the navigation bar form my Chinese given name.</p> */}
           {/* <p className="currently">Currently: Reading <i>Anna Karenina</i> by Leo Tolstoy, learning electric bass, writing a short story, making Hong Kong-style borscht, and playing <i>The Legend of Zelda: Tears of the Kingdom</i>.</p> */}
           {/* <h3>Experience</h3>
